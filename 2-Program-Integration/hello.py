fec68955d58a7b7531360ea33eb78adb59a3c7b7
shrish <adhikari.shrish1@gmail.com>
2024-06-30 20:10:38 +0545

    Added guage

--- 2-Program-Integration/hello.py
+++ 2-Program-Integration/hello.py
@@ -1,15 +1,20 @@
 import http.server
 import random
-from prometheus_client import start_http_server, Counter
+import time
+from prometheus_client import start_http_server, Counter, Gauge
 
 REQUESTS = Counter("hello_worlds_total", "Hello Worlds requested.")
 EXCEPTIONS = Counter("hello_world_exceptions_total", "Exceptions serving Hello World.")
+INPROGRESS = Gauge("hello_worlds_inprogress", "Number of Hello Worlds in progress.")
+LAST = Gauge("hello_world_last_time_seconds", "The Last time a Hello World was served.")
 
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     # @EXCEPTIONS.count_exceptions()
+    @INPROGRESS.track_inprogress()
     def do_GET(self):
         REQUESTS.inc()
+        # INPROGRESS.inc()
         with EXCEPTIONS.count_exceptions():
             if random.random() < 0.2:
                 raise Exception
@@ -17,6 +22,10 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
 
         self.end_headers()
         self.wfile.write(b"Hello World!!")
+        # use as `time() - hello_world_last_time_seconds`
+        # LAST.set(time.time())
+        LAST.set_to_current_time()  # set the guage to current time stamp
+        # INPROGRESS.dec()
 
 
 if __name__ == "__main__":
